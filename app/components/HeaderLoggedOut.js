0fd57ffdccd51d1fc7fcee03f619a3ece242882d
Travis Davis <skoolboi434@gmail.com>
2026-07-26 19:58:38 -0400

    Header login / logout functions added

---
+++ app/components/HeaderLoggedOut.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const HeaderLoggedOut = props => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  async function submitHandler(e) {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8080/login', { username, password });
+      if (response.data) {
+        localStorage.setItem('complexappToken', response.data.token);
+        localStorage.setItem('complexappUsername', response.data.username);
+        localStorage.setItem('complexappAvatar', response.data.avatar);
+        props.setLoggedIn(true);
+      } else {
+        console.log('Incorrect username or password.');
+      }
+    } catch (e) {
+      console.log('There was a problem.');
+    }
+  }
+
+  return (
+    <form onSubmit={submitHandler} className='mb-0 pt-2 pt-md-0'>
+      <div className='row align-items-center'>
+        <div className='col-md mr-0 pr-md-0 mb-3 mb-md-0'>
+          <input onChange={e => setUsername(e.target.value)} name='username' className='form-control form-control-sm input-dark' type='text' placeholder='Username' autoComplete='off' />
+        </div>
+        <div className='col-md mr-0 pr-md-0 mb-3 mb-md-0'>
+          <input onChange={e => setPassword(e.target.value)} name='password' className='form-control form-control-sm input-dark' type='password' placeholder='Password' />
+        </div>
+        <div className='col-md-auto'>
+          <button className='btn btn-success btn-sm'>Sign In</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default HeaderLoggedOut;
--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Container from './Container';
 import Page from './Page';
 
 const HomeGuest = () => {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  async function submitHandler(e) {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:8080/register', { username, email, password });
+
+      console.log('User was successfully created.');
+    } catch (e) {
+      console.log(e.response.data);
+    }
+  }
+
   return (
     <Page title='Home Guest' wide={true}>
       <div className='row align-items-center'>
@@ -11,24 +27,24 @@ const HomeGuest = () => {
           <p className='lead text-muted'>Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
         </div>
         <div className='col-lg-5 pl-lg-5 pb-3 py-lg-5'>
-          <form>
+          <form onSubmit={submitHandler}>
             <div className='form-group'>
               <label htmlFor='username-register' className='text-muted mb-1'>
                 <small>Username</small>
               </label>
-              <input id='username-register' name='username' className='form-control' type='text' placeholder='Pick a username' autoComplete='off' />
+              <input onChange={e => setUsername(e.target.value)} id='username-register' name='username' className='form-control' type='text' placeholder='Pick a username' autoComplete='off' />
             </div>
             <div className='form-group'>
               <label htmlFor='email-register' className='text-muted mb-1'>
                 <small>Email</small>
               </label>
-              <input id='email-register' name='email' className='form-control' type='text' placeholder='you@example.com' autoComplete='off' />
+              <input onChange={e => setEmail(e.target.value)} id='email-register' name='email' className='form-control' type='text' placeholder='you@example.com' autoComplete='off' />
             </div>
             <div className='form-group'>
               <label htmlFor='password-register' className='text-muted mb-1'>
                 <small>Password</small>
               </label>
-              <input id='password-register' name='password' className='form-control' type='password' placeholder='Create a password' />
+              <input onChange={e => setPassword(e.target.value)} id='password-register' name='password' className='form-control' type='password' placeholder='Create a password' />
             </div>
             <button type='submit' className='py-3 mt-4 btn btn-lg btn-success btn-block'>
               Sign up for ComplexApp
