874fe1598bd991fa98980ae89c24097f9081ad41
Travis Davis <skoolboi434@gmail.com>
2026-07-27 10:56:43 -0400

    Single post markup updated with data from DB

--- app/components/Page.js
+++ app/components/Page.js
@@ -5,7 +5,7 @@ const Page = props => {
   useEffect(() => {
     document.title = `${props.title} | Complex App`;
     window.scroll(0, 0);
-  }, []);
+  }, [props.title]);
   return <Container wide={props.wide}>{props.children}</Container>;
 };
 
