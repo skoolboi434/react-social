c66961d61b981786aa77e80cecf8e45ede5df4f1
Travis Davis <skoolboi434@gmail.com>
2026-07-27 00:07:07 -0400

    User posts added to profile

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,6 +3,7 @@ import Page from './Page';
 import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 import StateContext from '../StateContext';
+import ProfilePosts from './ProfilePosts';
 
 const Profile = () => {
   const { username } = useParams();
@@ -52,20 +53,7 @@ const Profile = () => {
         </Link>
       </div>
 
-      <div className='list-group'>
-        <Link to='#' className='list-group-item list-group-item-action'>
-          <img className='avatar-tiny' src='https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128' /> <strong>Example Post #1</strong>
-          <span className='text-muted small'>on 2/10/2020 </span>
-        </Link>
-        <Link to='#' className='list-group-item list-group-item-action'>
-          <img className='avatar-tiny' src='https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128' /> <strong>Example Post #2</strong>
-          <span className='text-muted small'>on 2/10/2020 </span>
-        </Link>
-        <Link to='#' className='list-group-item list-group-item-action'>
-          <img className='avatar-tiny' src='https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128' /> <strong>Example Post #3</strong>
-          <span className='text-muted small'>on 2/10/2020 </span>
-        </Link>
-      </div>
+      <ProfilePosts />
     </Page>
   );
 };
---
+++ app/components/ProfilePosts.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
+
+const ProfilePosts = props => {
+  const { username } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await axios.get(`/profile/${username}/posts`);
+        setPosts(response.data);
+        setIsLoading(false);
+      } catch (e) {
+        console.log('There was a problem.');
+      }
+    }
+    fetchPosts();
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <div className='list-group'>
+      {posts.map(post => {
+        const date = new Date(post.createdDate);
+        const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+        return (
+          <Link key={post._id} to={`/post/${post._id}`} className='list-group-item list-group-item-action'>
+            <img className='avatar-tiny' src={post.author.avatar} /> <strong>{post.title}</strong> <span className='text-muted small'>on {dateFormatted} </span>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ProfilePosts;
